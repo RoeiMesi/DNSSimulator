eb2bdfca7b404b1302a65e87e23e6c2fbf5eba9f
Dean Cohen <Dean5102000@gmail.com>
2024-11-13 19:15:45 +0200

    Modified the UDP server file and added an unfinished version of PART A report

--- udpServer.py
+++ udpServer.py
@@ -1,9 +1,12 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind('', 12345)
+s.bind(('0.0.0.0', 12345))  # Binding to all available interfaces on port 12345
+
+# print("Server is listening for incoming messages...")
 
 while True:
     data, addr = s.recvfrom(1024)
     print(str(data), addr)
+    # print("Received message:", str(data), "from", addr)
     s.sendto(data.upper(), addr)
